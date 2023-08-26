61b0cde4a07262d57343fd76c09a780827a4f1c6
jiwoong <jiwoongmun@gmail.com>
2023-08-27 02:02:14 +0900

    app retreive

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, render_template, url_for, redirect, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import time
-import json
-from flask_cors import CORS
-
-
 
 #############################
 ######initial set up ########
@@ -17,7 +13,6 @@ app.config['SECRET_KEY'] = 'mysecretkey'
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-CORS(app)
 
 db = SQLAlchemy(app)
 Migrate(app, db)
@@ -52,33 +47,21 @@ def uk_details():
 def us_details():
     return render_template('sub2/sub2-us.html')
 
-@app.route("/db_save", methods=['GET', 'POST'])
-def db_save():
-    param = json.loads(request.data)
-    print(param)
-    # base64_data = param['keyword'].split(':')[1]  # Remove the "data:image/png;base64," prefix
-    base64_data = param['keyword']
-    print(base64_data)
-    new_item = image_table(base64_data)
-    db.session.add(new_item)
-    db.session.commit()
-    return json.dumps(base64_data,ensure_ascii=False)
-
 @app.route('/archive', methods=['GET', 'POST'])
 def archive():
-
-    # if (data):
-        # new_item = image_table(data)
-        # db.session.add(new_item)
-        # db.session.commit()
-    #     images = image_table.query.order_by(image_table.id.desc()).all()
-        
-    # else: 
-    #     data = request.form.get('image')
-    #     if (data):
-    #         new_item = image_table(data)
-    #         db.session.add(new_item)
-    #         db.session.commit()
+    data = request.form.get('image')
+    print(data)
+    if (data):
+        new_item = image_table(data)
+        db.session.add(new_item)
+        db.session.commit()
+    else: 
+        time.sleep(2)
+        data = request.form.get('image')
+        if (data):
+            new_item = image_table(data)
+            db.session.add(new_item)
+            db.session.commit()
     images = image_table.query.order_by(image_table.id.desc()).all()
     return render_template('sub3/sub3.html', images=images)
 
