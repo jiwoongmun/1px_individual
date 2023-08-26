6ef7e80e97830a0f99640ce763d676e619f3dcfd
jiwoong <jiwoongmun@gmail.com>
2023-08-26 21:49:22 +0900

    fix

--- static/js/sub3.js
+++ static/js/sub3.js
@@ -74,7 +74,7 @@ save.addEventListener('click', (e) => {
     fetch('/archive', {
         method: 'POST',
         body: formData
-    }).then(window.location.reload()
+    }).then( location.href("{{(url_for('archive'))}}")
     );
         // 페이지 reload해서 보여줘야해
     })
